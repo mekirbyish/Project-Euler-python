adca73585b89418c82a5aabf1d45bfdde2f5cb83
mamcgrail <48732118+mamcgrail@users.noreply.github.com>
2021-01-24 16:33:46 -0500

    Create largest_palindrome_product.py

---
+++ largest_palindrome_product.py
@@ -0,0 +1,16 @@
+a=100
+b=100
+d=0
+x=0
+while a<1000:
+    z=b
+    while b<1000:
+        c=a*b
+        if str(c)==str(c)[::-1]:
+            d=int(c)
+            if d>x:
+                x=d
+        b+=1
+    b=z+1
+    a+=1
+print(x)
