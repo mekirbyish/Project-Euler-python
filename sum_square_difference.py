da4183b366fd9a1788928c8f2039f4df85d5a44e
mamcgrail <48732118+mamcgrail@users.noreply.github.com>
2021-01-30 12:51:21 -0500

    Update sum_square_difference.py

--- sum_square_difference.py
+++ sum_square_difference.py
@@ -1,10 +1,21 @@
+#should return 2640
+#a=10
+
+a=100
 sum_squares=0
 square_sums=0
 i=1
-while i<=100:
+
+#make list of numbers below limit, and another list of each's square summed
+while i<=a:
     square_sums+=i
     sum_squares+=pow(i,2)
     i+=1
+
+#square the sum of all the numbers below the limit
 square_sums=pow(square_sums,2)
+
+#find difference of these lists
 difference=square_sums-sum_squares
+
 print(difference)
