f966d57dbf541ce0f8a1a6c12cb460a1bd0e966c
mamcgrail <48732118+mamcgrail@users.noreply.github.com>
2021-01-30 12:51:30 -0500

    Update largest_prime_factor.py

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,5 +1,6 @@
 #a=13195
 #prime factors are 5, 7, 13 and 29
+
 a=600851475143
 prime_numbers=[]
 is_prime=True
@@ -8,16 +9,13 @@ prime_factors=[]
 
 #create prime numbers
 while i <= a/2:
-    #print("i is",i)
     is_prime=True
     for x in prime_numbers:
         if i%x==0:
             is_prime=False
-            #print(i, "is not prime")
             break
     if is_prime==True:
         prime_numbers.append(i)
-        #print(i, "is prime")
     i+=1
 
 #find prime factors of number
