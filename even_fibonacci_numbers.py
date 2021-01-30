a504db7372ae8033905693aa1bde09e806c45e7d
mamcgrail <48732118+mamcgrail@users.noreply.github.com>
2021-01-30 12:51:13 -0500

    Update even_fibonacci_numbers.py

--- even_fibonacci_numbers.py
+++ even_fibonacci_numbers.py
@@ -1,19 +1,20 @@
-#fib_start_1=1
-#fib_start_2=2
-#print(fib_start_1)
-#print(fib_start_2)
 fib_prev=0
 fib_curr=1
 fib_next=0
 evens=[]
 sum=0
+
+#would be better as recursion
 while fib_curr<4000000:
-    #print(fib_curr)
+    #check if it's even
     if fib_curr%2==0:
         evens.append(fib_curr)
     fib_next=fib_prev+fib_curr
     fib_prev=fib_curr
     fib_curr=fib_next
+
+#sum even fibs
 for x in evens:
     sum+=x
-print("sum is ",sum)
+
+print(sum)
