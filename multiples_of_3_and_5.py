94560b4c07a65ae41ce4a98c2b572f9d5c1181af
mamcgrail <48732118+mamcgrail@users.noreply.github.com>
2021-01-30 12:51:25 -0500

    Update multiples_of_3_and_5.py

--- multiples_of_3_and_5.py
+++ multiples_of_3_and_5.py
@@ -1,25 +1,18 @@
-#example
-mult_3_5=[]
-i = 1
-while i < 10:
-    if i%3==0 or i%5==0:
-        mult_3_5.append(i)
-    i += 1
-#should get 3, 5, 6, 9
-sum=0
-for x in mult_3_5:
-    sum+=x
-print(sum)
-#should get 23
+#test number, multiples of 3, 5, 6, 9 and sum of 23
+#a=10
 
-#solution
+a=1000
 mult_3_5=[]
 i = 1
-while i < 1000:
+sum=0
+
+#find multiples of 3 and 5 below limit
+while i < a:
     if i%3==0 or i%5==0:
         mult_3_5.append(i)
     i += 1
-sum=0
+
+#sum multiples
 for x in mult_3_5:
     sum+=x
 print(sum)
