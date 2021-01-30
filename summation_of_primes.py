039753528a06ccd4cc6bd817de3f0efc01bba99d
mamcgrail <48732118+mamcgrail@users.noreply.github.com>
2021-01-30 12:57:28 -0500

    Create summation_of_primes.py

---
+++ summation_of_primes.py
@@ -0,0 +1,25 @@
+#technically will work, but will run for like a month
+
+#return 17
+#a=10
+
+a=2000000
+prime_numbers=[]
+sum=0
+i=2
+
+while i < a:
+    print("i is",i)
+    is_prime=True
+    for x in prime_numbers:
+        if i%x==0:
+            is_prime=False
+            #print(i, "is not prime")
+            break
+    if is_prime==True:
+        prime_numbers.append(i)
+        #print(i, "is prime")
+    i+=1
+for x in prime_numbers:
+    sum+=x
+print(sum)
