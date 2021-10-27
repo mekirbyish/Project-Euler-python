f8b7ab6c223d37b1ca019475c1b1c90a772ac826
mekirbyish <77480462+mekirbyish@users.noreply.github.com>
2021-10-27 12:20:17 -0400

    Update largest_prime_factor.py

--- largest_prime_factor.py
+++ largest_prime_factor.py
@@ -1,29 +1,29 @@
 #a=13195
+#given number 13195
 #prime factors are 5, 7, 13 and 29
 
 a=600851475143
-prime_numbers=[]
-is_prime=True
-i=2
+
 prime_factors=[]
-
-#create prime numbers
-while i <= a/2:
-    is_prime=True
-    for x in prime_numbers:
-        if i%x==0:
-            is_prime=False
-            break
-    if is_prime==True:
-        prime_numbers.append(i)
-    i+=1
-
-#find prime factors of number
-for x in prime_numbers:
-    if a%x==0:
-        prime_factors.append(x)
-
-#find largest prime factor
+#https://www.pythonpool.com/prime-factorization-python/
+import math
+def primefactors(n):
+    #n odd
+    while n%2 == 0:
+        #print(2)
+        prime_factors.append(2)
+        n /= 2
+    #n even
+    for i in range(3,int(math.sqrt(n))+1,2):
+        while(n%i == 0):
+            #print(i)
+            prime_factors.append(i)
+            n /= i
+    if n>2:
+        #print(n)
+        prime_factors.append(n)
+n=int(input("Enter n:"))
+primefactors(n)
 prime_factors.sort()
 prime_factors.reverse()
-print(prime_factors[0])
+print("Largest factor: " + str(prime_factors[0]))
